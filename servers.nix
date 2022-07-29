06353f72e1f101d0ff5b3126b64ecdd43a7a83cc
Knarkzel <knarkzel@gmail.com>
2022-07-29 12:52:05 +0200

    Added service

--- servers.nix
+++ servers.nix
@@ -1,5 +1,9 @@
 let
   pkgs = import (fetchTarball "https://github.com/knarkzel/nixpkgs/archive/043de04db8a6b0391b3fefaaade160514d866946.tar.gz") {};
+  paste = fetchTarball {
+    url = "https://github.com/JJJollyjim/nixos-flask-example/archive/refs/heads/master.tar.gz";
+    sha256 = "02j802chsba7q4fmd6mzk59x1gywdpm210x1m4l44c219bxapl4r";
+  };
 in {
   network.pkgs = pkgs;
 
@@ -11,6 +15,7 @@ in {
   }: {
     imports = [
       (modulesPath + "/virtualisation/openstack-config.nix")
+      "${paste}/service.nix"
     ];
 
     # Morph options
@@ -20,15 +25,17 @@ in {
     # Environment
     networking.hostName = name;
     system.stateVersion = "22.05";
-    environment.systemPackages = with pkgs; [git];
 
     # oddharald.xyz
-    networking.firewall.allowedTCPPorts = [80];
+    networking.firewall.allowedTCPPorts = [80 8080];
     services.nginx = {
       enable = true;
       virtualHosts."oddharald.xyz" = {
         root = "/var/oddharald.xyz";
       };
     };
+
+    # paste service
+    services.paste.enable = true;
   };
 }
