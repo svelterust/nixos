0e608f6c970fc4763e67d72b54365395481d4311
Knarkzel <knarkzel@gmail.com>
2024-08-05 15:02:08 +0200

    .no screen

--- scripts/screensaver.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-LOCK_SCREEN_AFTER=600 # seconds
-SUSPEND_COMPUTER_AFTER=900 # seconds
-
-swayidle -w timeout $LOCK_SCREEN_AFTER 'swaylock -f' \
-            timeout $SUSPEND_COMPUTER_AFTER 'systemctl suspend' \
-            before-sleep 'swaylock -f' &
